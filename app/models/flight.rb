9dda610c6e9d096f32b73f1d0c74b4e8aec5ff47
Boston <bostoncessary@gmail.com>
2024-03-15 03:54:23 -0700

    add do block to collection_radio_buttons to add class to each radio label for styling

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,6 +6,10 @@ class Flight < ApplicationRecord
     takeoff.strftime("%m/%d/%Y")
   end
 
+  def display_info
+    "#{departure_airport.code} to #{arrival_airport.code}. Takeoff: #{takeoff_formatted}"
+  end
+
   def self.search(departure_airport, arrival_airport, year, month, date)
     if year != nil
       date = Time.new(year, month, date)
